15d27ff64a9ca7eb945bf83eb93dc33e20ece874
Titanium115 <70747548+Titanium115@users.noreply.github.com>
2021-05-31 09:51:50 +0200

    Add files via upload

---
+++ Projet Final grand oral/Dungeon Collectables.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Dungeon Collectables" tilewidth="32" tileheight="32" tilecount="45" columns="9">
+ <image source="Dungeon Collectables.png" width="288" height="176"/>
+</tileset>
